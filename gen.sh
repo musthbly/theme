855561706cd35f6d7e50e33b2d4ac20f44edee5c
musthbly <musthbly@pm.me>
2022-03-17 18:23:56 -0400

    Auto-generate Alacritty themes

--- gen.sh
+++ gen.sh
@@ -1,21 +1,64 @@
 #!/usr/bin/env dash
-cd pub || exit
-mogrify -fill '#1b1b1b' -draw 'rectangle 0,0 16,16' d0.png
-mogrify -fill '#ab8983' -draw 'rectangle 0,0 16,16' d1.png
-mogrify -fill '#8f937b' -draw 'rectangle 0,0 16,16' d2.png
-mogrify -fill '#a38c7c' -draw 'rectangle 0,0 16,16' d3.png
-mogrify -fill '#899297' -draw 'rectangle 0,0 16,16' d4.png
-mogrify -fill '#a68991' -draw 'rectangle 0,0 16,16' d5.png
-mogrify -fill '#899390' -draw 'rectangle 0,0 16,16' d6.png
-mogrify -fill '#c6c6c6' -draw 'rectangle 0,0 16,16' d7.png
-mogrify -fill '#919191' -draw 'rectangle 0,0 16,16' d8.png
-mogrify -fill '#262626' -draw 'rectangle 0,0 16,16' l0.png
-mogrify -fill '#765752' -draw 'rectangle 0,0 16,16' l1.png
-mogrify -fill '#5c614b' -draw 'rectangle 0,0 16,16' l2.png
-mogrify -fill '#6f5a4b' -draw 'rectangle 0,0 16,16' l3.png
-mogrify -fill '#576065' -draw 'rectangle 0,0 16,16' l4.png
-mogrify -fill '#72575f' -draw 'rectangle 0,0 16,16' l5.png
-mogrify -fill '#57605e' -draw 'rectangle 0,0 16,16' l6.png
-mogrify -fill '#ffffff' -draw 'rectangle 0,0 16,16' l7.png
-mogrify -fill '#5e5e5e' -draw 'rectangle 0,0 16,16' l8.png
-mogrify -strip ./*.png
+
+black=1b1b1b
+red=ab8983
+green=8f937b
+yellow=a38c7c
+blue=899297
+magenta=a68991
+cyan=899390
+white=c6c6c6
+grey=919191
+
+mogrify pub/d0.png -draw 'rectangle 0,0 16,16' -fill "#$black"
+mogrify pub/d1.png -draw 'rectangle 0,0 16,16' -fill "#$red"
+mogrify pub/d2.png -draw 'rectangle 0,0 16,16' -fill "#$green"
+mogrify pub/d3.png -draw 'rectangle 0,0 16,16' -fill "#$yellow"
+mogrify pub/d4.png -draw 'rectangle 0,0 16,16' -fill "#$blue"
+mogrify pub/d5.png -draw 'rectangle 0,0 16,16' -fill "#$magenta"
+mogrify pub/d6.png -draw 'rectangle 0,0 16,16' -fill "#$cyan"
+mogrify pub/d7.png -draw 'rectangle 0,0 16,16' -fill "#$white"
+mogrify pub/d8.png -draw 'rectangle 0,0 16,16' -fill "#$grey"
+
+sed -i.bak alacritty/dark.yml \
+  -e "s/black: \&black .*/black: \&black '#$black'/g" \
+  -e "s/red: \&red .*/red: \&red '#$red'/g" \
+  -e "s/green: \&green .*/green: \&green '#$green'/g" \
+  -e "s/yellow: \&yellow.*/yellow: \&yellow '#$yellow'/g" \
+  -e "s/blue: \&blue .*/blue: \&blue '#$blue'/g" \
+  -e "s/magenta: \&magenta .*/magenta: \&magenta '#$magenta'/g" \
+  -e "s/cyan: \&cyan .*/cyan: \&cyan '#$cyan'/g" \
+  -e "s/white: \&white .*/white: \&white '#$white'/g" \
+  -e "s/grey: \&grey .*/grey: \&grey '#$grey'/g"
+
+black=262626
+red=765752
+green=5c614b
+yellow=6f5a4b
+blue=576065
+magenta=72575f
+cyan=57605e
+white=ffffff
+grey=5e5e5e
+
+mogrify pub/l0.png -draw 'rectangle 0,0 16,16' -fill "#$black"
+mogrify pub/l1.png -draw 'rectangle 0,0 16,16' -fill "#$red"
+mogrify pub/l2.png -draw 'rectangle 0,0 16,16' -fill "#$green"
+mogrify pub/l3.png -draw 'rectangle 0,0 16,16' -fill "#$yellow"
+mogrify pub/l4.png -draw 'rectangle 0,0 16,16' -fill "#$blue"
+mogrify pub/l5.png -draw 'rectangle 0,0 16,16' -fill "#$magenta"
+mogrify pub/l6.png -draw 'rectangle 0,0 16,16' -fill "#$cyan"
+mogrify pub/l7.png -draw 'rectangle 0,0 16,16' -fill "#$white"
+mogrify pub/l8.png -draw 'rectangle 0,0 16,16' -fill "#$grey"
+mogrify -strip pub/*.png
+
+sed -i.bak alacritty/light.yml \
+  -e "s/black: \&black .*/black: \&black '#$black'/g" \
+  -e "s/red: \&red .*/red: \&red '#$red'/g" \
+  -e "s/green: \&green .*/green: \&green '#$green'/g" \
+  -e "s/yellow: \&yellow.*/yellow: \&yellow '#$yellow'/g" \
+  -e "s/blue: \&blue .*/blue: \&blue '#$blue'/g" \
+  -e "s/magenta: \&magenta .*/magenta: \&magenta '#$magenta'/g" \
+  -e "s/cyan: \&cyan .*/cyan: \&cyan '#$cyan'/g" \
+  -e "s/white: \&white .*/white: \&white '#$white'/g" \
+  -e "s/grey: \&grey .*/grey: \&grey '#$grey'/g"
